7ea450f8e76b66421f341954e5e6e0a0676afcc3
Christian Varela <christianvarela@faee.com.br>
2024-11-09 20:45:39 -0300

    build: initial project setup

---
+++ src/main/java/com/tax/registry/TaxRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.tax.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tax/registry/TaxRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tax.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
