8c2c8d2f7822a2bec37d0fc8e4df3c925cef48b9
Christian Varela <christianvarela@faee.com.br>
2024-11-10 19:31:38 -0300

    fix: controller for API testing

---
+++ src/main/java/com/tax/registry/controllers/PingController.java
@@ -0,0 +1,17 @@
+package com.tax.registry.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/ping")
+public class PingController {
+
+	@GetMapping
+	public ResponseEntity<Object> ping(@RequestParam(name = "message") String message) {
+		return ResponseEntity.ok("Ping -> " + message);
+	}
+}
